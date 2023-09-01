18d9ee42bfcf79a9af949ca6edf5c847f009860e
Sara <saracoppola996@gmail.com>
2023-09-01 16:07:59 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-
         Animal animal = new Animal("pesce");
-        Animal lion = new Lion("leone");
-        Animal cow = new Cow("mucca");
+        Lion lion = new Lion("leone");
+        Cow cow = new Cow("mucca");
 
         animal.animalSound();
         lion.animalSound();
